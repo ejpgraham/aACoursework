bac7b6232fe4b1b50b1fe071fcde89a9cf1f43ee
Chithra Venkatesan <chithra.venkatesan.5@gmail.com>
2017-03-17 15:30:59 -0400

    W3D5 Finish Searchable

--- W3D5/ActiveRecord/lib/02_searchable.rb
+++ W3D5/ActiveRecord/lib/02_searchable.rb
@@ -3,19 +3,37 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    questionized_keys =  params.keys.map{|name|name.to_s + "= ?"}.join(" AND ")
+    results = DBConnection.execute(<<-SQL, params.values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{questionized_keys}
+    SQL
+
+    self.parse_all(results)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable #extend adds class methods, not instance methods like includes
 end
 
 
-haskell_cats = Cat.where(:name => "Haskell", :color => "calico")
+# haskell_cats = Cat.where(:name => "Haskell", :color => "calico")
 # SELECT
 #   *
 # FROM
 #   cats
 # WHERE
 #   name = ? AND color = ?
+
+#
+# DBConnection.execute(<<-SQL, vals)
+#   INSERT INTO
+#   #{self.class.table_name} (#{col_names})
+#   VALUES
+#   (#{question_marks})
+# SQL
