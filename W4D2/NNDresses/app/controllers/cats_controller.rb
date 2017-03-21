034d4b82a702fd6daba4c3be10e6f1c55d49ebdf
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-03-21 14:44:14 -0400

    Finish cat controller

--- W4D2/NNDresses/app/controllers/cats_controller.rb
+++ W4D2/NNDresses/app/controllers/cats_controller.rb
@@ -15,7 +15,32 @@ class CatsController < ApplicationController
   end
 
   def create
-    
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cats_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
   end
 
 end
