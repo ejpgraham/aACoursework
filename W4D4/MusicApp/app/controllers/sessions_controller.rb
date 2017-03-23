9b6943adbacde2eb559b704d3b6c090d41e4653e
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-03-23 11:55:01 -0400

    Setup MusicApp Project, Finish Warmup

---
+++ W4D4/MusicApp/app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:user][:email],params[:user][:password])
+    if @user
+      log_in!(@user)
+      redirect_to user_url(@user)
+    else
+      render :new
+    end
+  end
+
+end
