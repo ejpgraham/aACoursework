a59408e57682671ce096f9f7b915aa8c97d37d5c
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-03-28 15:06:33 -0400

    Finish Phase 2

--- W5D2/skeleton/lib/controller_base.rb
+++ W5D2/skeleton/lib/controller_base.rb
@@ -2,31 +2,45 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
 require_relative './session'
+require 'active_support/inflector'
 
 class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise 'double render' if already_built_response?
+    @res.status = 302
+    @res.location = url
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise 'double render' if already_built_response?
+    @res['content-type'] = content_type
+    @res.write(content)
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    url = self.class.name.underscore
+    template = ERB.new
   end
 
   # method exposing a `Session` object
@@ -37,4 +51,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
