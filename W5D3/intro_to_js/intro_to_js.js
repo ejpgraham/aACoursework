88c10be5c50ed608525b7fbc4dde77e3896d4db7
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-03-29 12:31:54 -0400

    Finish two sum

--- W5D3/intro_to_js/intro_to_js.js
+++ W5D3/intro_to_js/intro_to_js.js
@@ -1,23 +1,40 @@
-console.log("hihihi")
+console.log("hihihi");
 
 
-function removeDuplicates(arr) {
-  const holder = [];
+function myUniq(arr) {
+  let result = [];
+  for(let i=0; i<arr.length; i++){
 
-  for(let i=0; i < arr.length; i++) {
-    if (holder.includes(arr[i])){
-      continue;
-    } else {
-      holder.push(arr[i]);
+    if(!(result.includes(arr[i]))){
+      result.push(arr[i]);
     }
   }
-  return holder;
+  return result;
 }
 
-console.log(removeDuplicates([1,2,3,4,1,2]));
-console.log(removeDuplicates([1,2,3,4,1,2,"siesta", "meowth", "siesta"]));
+console.log(myUniq([1,2,3,4,1,2]));
+console.log(myUniq([1,2,3,4,1,2,"siesta", "meowth", "siesta"]));
 
 
-function two_sum(arr) {
-  const 
+function twoSum(arr) {
+  let result = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    let firstNum = arr[i];
+
+    for (var j = (i+1); j < arr.length; j++) {
+      let secondNum = arr[j];
+      if ((firstNum + secondNum) === 0){
+        result.push([i,j]);
+      }
+    }
+  }
+  return result;
+}
+
+console.log(JSON.stringify(twoSum([-1, 0, 2, -2, 1])));
+
+
+function myTranspose(arr) {
+  let results = new Array(arr.length).fill(new Array(arr[0].length))
 }
